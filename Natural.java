f3629d2bb206eac82565fa9eaf753ee227cde4a6
jesusble <keziahblessy33@gmail.com>
2018-08-06 14:41:38 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Natural
+{
+public static void main(String args[])
+{
+System.out.println("enter the limit");
+Scanner sc=new Scanner(System.in)
+int a=sc.nextInt();
+int sum=0;
+for(i=0;i<a;i++)
+{
+sum=sum+i;
+System.out.println(sum);
+}
+}
+}
+
