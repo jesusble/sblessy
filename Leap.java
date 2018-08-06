5a1129b3f71be3ccd52a0dbdab13a210434c614b
jesusble <keziahblessy33@gmail.com>
2018-08-06 14:34:06 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java util.*;
+class Leap
+{
+public class void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the year you want to check");
+int a=sc.nextInt();
+if(a%4==0||a%100==0);
+{
+System.out.println("leap year");
+}
+else
+{
+System.out.println("not a leap year");
+}
+}
+}
