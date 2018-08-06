82c3a774795daaf223a5d1146fa494142f6f58ed
jesusble <keziahblessy33@gmail.com>
2018-08-06 14:24:37 +0530

    Create Alphabhet.java

---
+++ Alphabhet.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class Alphabhet
+{
+public static void main(String args[])
+{
+System.out.println("enter a character");
+Scanner sc=new Scanner(System.in);
+char n=sc.next().charAt(0);
+if(A||B||C||D||E||F||G||H||I||J||K||L||M||N||O||P||Q||R||S||T||U||V||W||X||Y||Z)
+{
+System.out.println("character is alphabhet");
+}
+else
+{
+System.out.println("character is not alphabhet");
+}
+}
+}
