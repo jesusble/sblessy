cf16bdb3049037bb795b1d558717c69631c66107
jesusble <keziahblessy33@gmail.com>
2018-08-06 13:59:52 +0530

    Create blessy.java

---
+++ blessy.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Word
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number");
+int n=sc.nextInt();
+for(i=0;i<n;i++)
+{
+System.out.println("HELLO");
+}
+}
+}
